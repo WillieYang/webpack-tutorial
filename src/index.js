9d87aa855f987e4800dc2f501ffbad2fefe225df
yangs <yangs@gisuni.com>
2019-02-28 11:30:53 +0800

    Make use of css-loader and mini-extract-css library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-console.log('Hello World Webpack');
+import style from "./main.css";
 
+console.log('Hello World Webpack');
 const arr = [1, 2, 3];
 const iAmJavascriptES6 = () => console.log(...arr);
 window.iAmJavascriptES6 = iAmJavascriptES6;
